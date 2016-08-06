851f52cb7cbb620b4039808cec630a38459c0b63
ashishbansal1975 <ashish_bansal01@yahoo.com>
2016-08-06 17:39:03 +0530

    Hi

---
+++ Session1Assignment3.java
@@ -0,0 +1,15 @@
+public class Session1Assignment3
+{
+    public static void main(String [] args)
+    {
+            int n1, n2;
+            long l1 = 30;
+            double d1;
+
+            n1 = l1;
+            n2 = d1;   
+            float num2 = 2;
+                                      
+     }
+
+}
